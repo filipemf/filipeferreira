6299ef87df49a9782eb15114d1bbe42c07316853
lipemf <filipemarquesf.dev@gmail.com>
2022-11-21 14:00:16 -0300

    Adicionando botão subir para cima

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import BackToTop from "./components/BackToTop";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -10,6 +11,7 @@ function App() {
   return (
     <div>
       <Navbar />
+      <BackToTop/>
       <Home />
       <About />
       <Skills />
---
+++ src/components/BackToTop.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react'
+import {SlArrowUp} from 'react-icons/sl'
+
+const BackToTop = () => {
+    const [backToTopButton, setBackToTopButton] = useState(false)
+
+    useEffect(()=>{
+        window.addEventListener('scroll', ()=>{
+            if(window.scrollY > 100){
+                setBackToTopButton(true)
+            }
+            else{
+                setBackToTopButton(false)
+            }
+        })
+    }, [])
+
+    const scrollUp = ()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+  return (
+    <div>
+      {backToTopButton && (
+        <button className='fixed bottom-[50px] z-[9999999] right-[50px] h-[50px] w-[50px] bg-[#18c8a8] border-solid border-2 border-white rounded' onClick={scrollUp} >
+            <SlArrowUp className='ml-[12px] text-[#fff]' size={20}/>
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default BackToTop
